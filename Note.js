11c6aee0200310669280bea365351d82daeae606
meowTarae <bae94157617@gmail.com>
2021-08-15 15:34:49 +0900

    08-15_15-34
    add mug-cup

---
+++ Note.js
@@ -0,0 +1,18 @@
+-중앙 배열-{
+position: absolute;
+top: 50%;
+left: 50%;
+transform: translate(-50%, -50%);
+  
+ =
+
+display: flex;
+justify-content: center;
+min-height: 100vh;
+align-items: center;
+}
+
+
+-글자 뒤에 배경이 오게끔~ webkit 활용!-{
+    -webkit-background-clip: text;
+}
